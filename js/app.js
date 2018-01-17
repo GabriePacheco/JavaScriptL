ec824bd75ea7191fe0d33759d6a5785cfb4ac004
Gabriel Pacheco <gabioh2012@gmail.com>
2018-01-16 21:48:24 -0500

    crear proyecto

--- js/app.js
+++ js/app.js
@@ -1,64 +1,168 @@
-var teclas = document.getElementsByClassName("tecla");
-var pantalla = document.getElementById("display");
+var cal = function (){
+	//Inicializacion de variables
+	var numero ; 
+	var nuemero2;// variable auxiliar 
+	var operacion; 
+	numero = 0;
+	numero2 =0;
+	operacion ="";
 
-var numero =0;
-var operacion = "" ;
-for (var i = 0; i<teclas.length; i++){
-	teclas[i].onclick =  function (){
-		if  (this.id != "on" && this.id != "raiz" && this.id != "sign" && this.id != "sign" && "raiz" && this.id != "dividido" && this.id != "por" && this.id != "."  && this.id != "menos" && this.id != "igual" && this.id != "mas" &&  this.id != "punto" ){
-			
-			if(pantalla.innerHTML == "0" || pantalla.innerHTML == "" ){
-				limpiarPantalla();
-				numero = this.id 	;
+	//agsino la pantalla a variable
+	var pantalla = document.getElementById("display");
 
-			}else if (numero.length < 8) {
-				numero+= this.id 	;
+	// obtendo las las teclas
+	var teclas = document.querySelectorAll(".tecla");
+
+	 // recoremos las teclas asignadoles funciones 
+	 for (var i = 0; i < teclas.length; i++ ){
+	 	//asigno funcion al para hacer pequeño la tecla al aplastar 
+	 	teclas[i].onmousedown = function (){
+	 		auxAnchoTecla =  this.style.width;
+	 		this.style.border= "double 0px black";
+	 		this.style.padding= "1%";
+		}
+
+		//Funciones para  restituir la tecla 
+		teclas[i].onmouseup = function (){
+			this.style.width=auxAnchoTecla;
+			this.style.border= "none";
+	 		this.style.padding= "0";
+
+		}
+		teclas[i].onmouseout = function (){
+			this.style.width=auxAnchoTecla;
+			this.style.border= "none";
+	 		this.style.padding= "0";
+
+		}
+
+		//agrego proceso de validacion de la tecla "Que tecla peciono (Clicl)"k
+		teclas[i].onclick = function (){
+			switch (this.id){
+				case "on": // telca de encendido limpia la pantalla y pone 0 a los numeros
+					numero = 0;
+					nuemero2=0;
+					mostrar (numero);
+				break;
+				case "punto": //al presionar puto 
+					if (pantalla.innerHTML.indexOf(".") < 0){// si no hay punto agregamos
+						numero += "." ;
+						mostrar(numero);
+					}
+
+				break;
+				case "sign":// cambiar de signo 
+					numero = numero * (-1);
+					mostrar (numero);
+				break;
+				case "mas": 
+					if (numero2 == 0 && operacion == "" ){
+						numero2=numero;
+						
+						
+					} else {
+						numero2 = igual(numero, numero2, operacion);
+					}
+					operacion = "suma";
+					numero="";
+					mostrar(numero);
+				break;
+				case "dividido": 
+					if (numero2 == 0 && operacion == "" ){
+						numero2=numero;
+						
+						
+					} else {
+						numero2 = igual(numero2, numero,operacion);
+					}
+					operacion = "divicion";
+					numero="";
+					mostrar(numero);
+				break;
+				case "por": 
+					if (numero2 == 0 && operacion == "" ){
+						numero2=numero;
+						
+						
+					} else {
+						numero2 = igual(numero, numero2, operacion);
+					}
+					operacion = "por";
+					numero="";
+					mostrar(numero);
+	
+
+				break;
+
+				case "menos": 
+					if (numero2 == 0 && operacion == "" ){
+						numero2=numero;
+						
+						
+					} else {
+						numero2 = igual(numero2,numero, operacion);
+					}
+					operacion = "resta";
+					numero="";
+					mostrar(numero);
+	
+
+				break;
+				case "igual":
+					numero = igual(numero, numero2, operacion);
+					console.log (numero , numero2, operacion);
+					numero2=0;
+					operacion ="";
+					mostrar (numero);
+
+				break;
+				default: //la tecla presionada es un numero.
+					if (pantalla.innerHTML=="0" || pantalla.innerHTML == ""){
+						//si la pantalla es 0 o esta vacía asisnamos el id tecla
+						numero = this.id;
+
+					} else if (pantalla.innerHTML.length < 8){ //si la pantalla tiene no tiene 8 dijitos  
+						numero+= this.id;//agragamos el numero 
+
+					}
+					mostrar(numero);	
+				break;
+
+
+
 			}
-			
-			pantalla.innerHTML = numero;
 
 		}
-		if (this.id == "igual" && operacion != "" ){
-			igual (numero2, numero, operacion);
-		}
 
-		if (this.id == "mas" && numero != 0){
-			limpiarPantalla();
-			numero2= numero;
-			numero = 0;
-			operacion = "mas";
+	 }
 
-		}
-		if (this.id == "menos" && numero != 0){
-			limpiarPantalla();
-			numero2= numero;
-			numero = 0;
-			operacion = "menos";
+	 //funcion para actualizar pantalla
+	 var mostrar = function (valor){
+	 	pantalla.innerHTML = valor;
+	 }
 
-		}
-		if  (this.id == "on" ){
-			pantalla.innerHTML="0";	
-			numero=0;		
-			opracion ="";
-		}
-	}
-}
+	 //funcio que realiza el calculo 
+	 var igual = function (n1,n2,operacion){
+	 	if (operacion == "suma"){
+	 		return parseFloat(n1)+parseFloat(n2);
+	 	}
+	 	if (operacion == "resta"){
+	 		return parseFloat(n2)-parseFloat(n1);
+	 	}
+	 	if (operacion == "por"){
+	 		return parseFloat(n1)*parseFloat(n2);
 
-function limpiarPantalla(){
-	pantalla.innerHTML="";
-}
-function igual(n2,n1,operacion){
-	if (operacion == "mas"){
-		 resultado =parseFloat(n1) +parseFloat(n2) ;
-		 numero = resultado;
-		 numero2 = 0;
-	}
-	if (operacion == "menos"){
-		 resultado =parseFloat(n2) - parseFloat(n1) ;
-		 numero = resultado;
-		 numero2 = 0;
-	}
-	pantalla.innerHTML= resultado;
+	 	}
+	 	if (operacion == "divicion"){
+	 		return parseFloat(n2)/parseFloat(n2);
+
+	 	}
+
+	 }
+
+
 
 
 }
+
+cal();
